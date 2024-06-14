1bd394cc432bb86535d17bd2317302f6fe889d40
Henrique Ferraz <henriqueferraz@ofnet.com.br>
2024-06-13 21:52:43 -0300

    init

---
+++ src/app/portal/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+        <div className="flex items-center flex-col justify-center min-h-screen">
+            <h1>Página de login</h1>
+        </div>
+    )
+}
---
+++ src/app/portal/page.tsx
@@ -0,0 +1,7 @@
+export default function PortaPage() {
+    return (
+        <div className="flex items-center flex-col justify-center min-h-screen">
+            <h1>Home do Portal</h1>
+        </div>
+    )
+}
